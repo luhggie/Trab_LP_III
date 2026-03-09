85540cec70af3cedf14f0d93614d4da7d2add89d
Lu Garcia <131422402+luhggie@users.noreply.github.com>
2026-03-09 08:54:29 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI(title="Backend - Projeto Lab Prog III")
 
+#Checagem de status
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+#Autenticação
+usuarios = []
+
+#Cadastro
+class CadastroRequest(BaseModel):
+    email: str
+    nome: str
+    senha: str
+
+@app.post("/cadastro")
+def cadastro(dados: CadastroRequest):
+    for usuario in usuarios:
+        if usuario["email"] == dados.email:
+            raise HTTPException(
+                status_code=400,
+                detail="E-mail já cadastrado"
+            )
+    novo_usuario = {
+        "email": dados.email,
+        "nome": dados.nome,
+        "senha": dados.senha
+    }
+
+    usuarios.append(novo_usuario)
+
+    return {
+        "mensagem": "Usuário cadastrado com sucesso",
+        "usuario": {
+            "nome": dados.nome,
+            "email": dados.email
+        }
+    }
+
+#LogIn
+class LoginRequest(BaseModel): #Modelo de JSON enviado pelo usuario
+    email: str
+    senha: str
+
+@app.post("/login")
+def login(dados: LoginRequest):
+    for usuario in usuarios:
+        if usuario["email"] == dados.email and usuario["senha"] == dados.senha:
+            return {
+                "mensagem": "Login realizado com sucesso",
+                "usuario": usuario["nome"]
+            }
+        
+    raise HTTPException(
+        status_code=401,
+        detail="Email ou senha inválidos"
+    )
